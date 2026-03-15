9c26165c4cf0f0724c1edeab3568a3e798ab8fc6
timofey basuev <t.basuev@bronevik.com>
2026-03-16 00:04:06 +0500

    tatami 0.2.0

--- Casks/tatami.rb
+++ Casks/tatami.rb
@@ -1,6 +1,6 @@
 cask "tatami" do
-  version "0.1.1"
-  sha256 "5e3548553a61f125e8fe5b97503151b06d3b5764db2a9124dc2699024c6a57b8"
+  version "0.2.0"
+  sha256 "dc556ca04292cd8eb4ee2aef21bd93bd9addabc6e19ea922d06f63c89bb748aa"
 
   url "https://github.com/basuev/tatami/releases/download/v#{version}/tatami.zip"
   name "tatami"
