fe2b8db7b9a816eb33e3e31113445762f35cd9eb
timofey basuev <t.basuev@bronevik.com>
2026-03-18 12:48:58 +0500

    rename: tatami -> parket

--- Casks/parket.rb
+++ Casks/parket.rb
@@ -1,20 +1,20 @@
-cask "tatami" do
+cask "parket" do
   version "0.5.3"
   sha256 "2b9323175feab31384599f6a3762eb2c6f6f9f8cf026ab323bc496e9fc5eddfd"
 
-  url "https://github.com/basuev/tatami/releases/download/v#{version}/tatami.zip"
-  name "tatami"
+  url "https://github.com/basuev/parket/releases/download/v#{version}/parket.zip"
+  name "parket"
   desc "minimal dwm-like tiling window manager for macOS"
-  homepage "https://github.com/basuev/tatami"
+  homepage "https://github.com/basuev/parket"
 
   postflight do
-    app_path = "/Applications/tatami.app"
-    staged_app = "#{staged_path}/tatami.app"
+    app_path = "/Applications/parket.app"
+    staged_app = "#{staged_path}/parket.app"
 
     if Dir.exist?(app_path)
       system_command "/bin/cp",
-                     args: ["#{staged_app}/Contents/MacOS/tatami",
-                            "#{app_path}/Contents/MacOS/tatami"]
+                     args: ["#{staged_app}/Contents/MacOS/parket",
+                            "#{app_path}/Contents/MacOS/parket"]
       system_command "/bin/cp",
                      args: ["#{staged_app}/Contents/Info.plist",
                             "#{app_path}/Contents/Info.plist"]
@@ -32,7 +32,7 @@ cask "tatami" do
     end
   end
 
-  uninstall delete: "/Applications/tatami.app"
+  uninstall delete: "/Applications/parket.app"
 
   zap trash: []
 end
